201d77a697f1164ce1d64dbb30d2f7eec32096a7
Hyperloop <bombear1@gmail.com>
2017-03-22 17:39:28 +0100

    jumpingMan getting better

--- purecss.js
+++ purecss.js
@@ -1,3 +1,7 @@
 function pacman() {
      document.getElementById("container").innerHTML='<object type="text/html" data="pacman.html" ></object>';
 }
+
+function jumpingMan() {
+     document.getElementById("container").innerHTML='<object type="text/html" data="jumpingMan.html" ></object>';
+}
